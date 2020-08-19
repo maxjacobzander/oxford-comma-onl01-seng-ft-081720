ddd97d7640095c4b069e99f0a3b0cfd40dcd86d3
Max Zander <maxjacobzander@gmail.com>
2020-08-19 22:33:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
   if array.length == 2
-    array[-2] << " and "
+    array.join(" and ")
+  elsif array.length > 2
+    array_2 = array.pop
+    comma_array = array.join(", ")
+    comma_array << "and #{array_2}"
+    array.join(" ")
+  elsif array.length < 2
     array.join
-  if array.length > 2
-    array.insert(-2, "and")
   end
 end
-
-oxford_comma(array)
