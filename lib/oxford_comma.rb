47a1ba1a280cef2588deffd906d3e338593b3086
Max Zander <maxjacobzander@gmail.com>
2020-08-19 22:56:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    array_2 = array.pop
+    comma_array = array.join(", ")
+    comma_array << ", and #{array_2}"
+  elsif array.length < 2
+    array.join
+  end
 end
